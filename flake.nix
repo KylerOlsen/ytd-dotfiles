935b05fdf7ab10fecfed307f5b3a2fa7bd6cfbd5
Kyler Olsen <yrea2001@gmail.com>
2025-05-07 21:42:24 -0600

    Changed hostname

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     pkgs = nixpkgs.legacyPackages.${system};
   in {
     nixosConfigurations = {
-      nixos-ytd = lib.nixosSystem {
+      moonk4-l = lib.nixosSystem {
         inherit system;
         modules = [ ./sys/configuration.nix ];
       };
